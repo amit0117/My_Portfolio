144aaf27b832a74ca61713747a6ebcadac3e10ba
amit0117 <20je0117@cve.iitism.ac.in>
2023-06-16 16:32:27 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// creating typing animation using Typed.js Library
+var typed=new Typed(".typing",{
+    strings:["","web Developer.","competetive Programmer."],
+    typeSpeed:100,
+    BackSpeed:80,
+    cursorSpeed:10,
+    suffle:true,
+    backDelay:500,
+    loop:true,
+})
