96d0e21b3203e95873b8477f372d9feb62ac6809
amit0117 <20je0117@cve.iitism.ac.in>
2023-07-05 18:39:49 +0530

    added skills

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // creating typing animation using Typed.js Library
 var typed=new Typed(".typing",{
-    strings:[""," Web Developer."," Competetive Programmer."," Machine Learning Enthusiast."],
+    strings:["","Full Stack Web Developer."," Competetive Programmer."," Machine Learning Enthusiast."],
     typeSpeed:100,
     BackSpeed:40,
     cursorSpeed:20,
